d128ab06308bd84e3394d6046669607ec5db2bb6
Ashank Kunwar <140194740+ashkunwar@users.noreply.github.com>
2024-09-10 22:10:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from dotenv import load_dotenv
 
 # Set up environment variable for API key
 load_dotenv()
-os.environ["GOOGLE_API_KEY"] = os.getenv('GEMINI_API_KEY')
+os.environ["GOOGLE_API_KEY"] = os.getenv('api')
 
 # Initialize the model
 model = ChatGoogleGenerativeAI(model="gemini-pro", convert_system_message_to_human=True)
